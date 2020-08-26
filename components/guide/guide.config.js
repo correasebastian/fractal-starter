c6ad0d1d317fd317026945f3586d4a877e68cfa6
sebastian correa <sebastian@sawyereffect.com>
2020-08-26 14:39:56 -0400

    using config for guide

--- components/guide/guide.config.js
+++ components/guide/guide.config.js
@@ -1,35 +1,43 @@
 module.exports ={
     context:{
-        basics:[
-            {
-                url:'',
-                title:'The basics'
+        modules:[
+            {   
+                title:'Comforter Basics:',
+                cards:[
+                    {
+                        url:'',
+                        title:'The basics'
+                    },
+                    {
+                        url:'',
+                        title:'Fill'
+                    },
+                    {
+                        url:'',
+                        title:'Warmth Level'
+                    },
+                    {
+                        url:'',
+                        title:'Size'
+                    },
+                ]
             },
             {
-                url:'',
-                title:'Fill'
-            },
-            {
-                url:'',
-                title:'Warmth Level'
-            },
-            {
-                url:'',
-                title:'Size'
-            },
-        ],
-        moreInfo:[
-            {
-                url:'',
-                title:'How to Clean'
-            },
-            {
-                url:'',
-                title:'FAQ'
-            },
-            {
-                url:'',
-                title:'Reviews'
+                title:"More Info:",
+                cards:[
+                    {
+                        url:'',
+                        title:'How to Clean'
+                    },
+                    {
+                        url:'',
+                        title:'FAQ'
+                    },
+                    {
+                        url:'',
+                        title:'Reviews'
+                    }
+                ]
             }
         ]
     }
