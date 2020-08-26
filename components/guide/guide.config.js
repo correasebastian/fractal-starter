f542c792b4febb36ab493c95a2a779bd3d2bd297
sebastian correa <sebastian@sawyereffect.com>
2020-08-26 14:35:09 -0400

    using each

---
+++ components/guide/guide.config.js
@@ -0,0 +1,22 @@
+module.exports ={
+    context:{
+        basics:[
+            {
+                url:'',
+                title:'The basics'
+            },
+            {
+                url:'',
+                title:'Fill'
+            },
+            {
+                url:'',
+                title:'Warmth Level'
+            },
+            {
+                url:'',
+                title:'Size'
+            },
+        ]
+    }
+}
