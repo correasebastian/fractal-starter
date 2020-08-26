955cb08c62398dfd30df8f94919eaf8242a56164
sebastian correa <sebastian@sawyereffect.com>
2020-08-26 10:23:01 -0400

    guide-image--fill

--- components/guide-image/guide-image.config.js
+++ components/guide-image/guide-image.config.js
@@ -1,9 +1,10 @@
 
 module.exports = {
-	"context": {
-        "title": "What’s the difference between a Comforter and a Duvet Insert?",
-        "mainImage":"https://res.cloudinary.com/dg1knuetg/image/upload/w_1200,ar_1:1,c_fill,g_auto/v1598412568/duvet_w1xhq3.png",
-        "richText": `
+	context: {
+        subtitle:"The Basics",
+        title: "What’s the difference between a Comforter and a Duvet Insert?",
+        mainImage: "https://res.cloudinary.com/dg1knuetg/image/upload/w_1200,ar_1:1,c_fill,g_auto/v1598412568/duvet_w1xhq3.png",
+        richText: `
             <p >
                 Did you know that comforters and duvet inserts are essentially the same thing? Both are typically filled with down, feathers, or synthetic fibers, will keep you warm, and can be used with our duvet covers which include an array of fun solid colors, fashionable prints, and premium fabrics. Duvet covers can also be interchanged frequently, are easily washed, and protect your investment so it’s a win-win.  
                 All our comforters (and duvet inserts) have handy loops on all four corners that attach to our duvet cover ties to easily secure both in place.
@@ -12,5 +13,23 @@ module.exports = {
             src="https://res.cloudinary.com/dg1knuetg/image/upload/ar_335:188,c_fill/v1598448868/juice_h7ic1b.png" 
             alt="" >
         `
-    }
+    },
+    variants: [
+		{
+			name: "fill",
+            context: {
+                subtitle:"Fill",
+                title: "Down vs. Down Alternative",
+                mainImage: "https://res.cloudinary.com/dg1knuetg/image/upload/ar_1:1,c_fill/v1598451167/cotton.png",
+                richText: `
+                    <p>
+                        Warm and comfortable, lightweight and breathable, down is Mother Nature’s most efficient insulator. Down comforters are long-lasting and resilient if given proper care which makes them a good investment for your bed.
+                    </p>
+                    <img 
+                    src="https://res.cloudinary.com/dg1knuetg/image/upload/ar_335:188,c_fill/v1598451404/pink.png" 
+                    alt="" >
+                `
+            }
+		}
+	]
 };
