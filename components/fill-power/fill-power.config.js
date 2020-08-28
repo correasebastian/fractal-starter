6ebe062387c08de5e04bf0d21618ffed06d6c9ba
sebastian correa <sebastian@sawyereffect.com>
2020-08-28 12:13:20 -0400

    fill power mobile

---
+++ components/fill-power/fill-power.config.js
@@ -0,0 +1,28 @@
+module.exports={
+    context:{
+        cards:[
+            {
+                title:"550-550",
+                subtitle1:"GOOD",
+                p1:"Our 500-550 fill power down provides premium warmth at an affordable price.",
+                subtitle2:"Recommended:",
+                p2:"Lacrosse",
+            },
+            {
+                title:"600-650",
+                subtitle1:"Better",
+                p1:"A high-quality down with plump and resilient clusters. 600-650 fill power provides warmth and wonderful loft.",
+                subtitle2:"Recommended:",
+                p2:"Alberta",
+            }
+            ,
+            {
+                title:"700-750",
+                subtitle1:"Best",
+                p1:"Our most luxurious down from select cold climate geese. 700-750 fill power Hungarian goose down provides the largest down clusters with exceptional loft.",
+                subtitle2:"Recommended:",
+                p2:"Legends Luxury Royal",
+            }
+        ]
+    }
+}
