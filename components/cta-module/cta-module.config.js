6ea040213fa4faaac3694a7bbc32d9f9bca7bf97
dJimenez04 <djimenez@unitedvirtualities.com>
2020-08-28 15:50:11 -0600

    [TCS-5] adding cta files component

---
+++ components/cta-module/cta-module.config.js
@@ -0,0 +1,19 @@
+module.exports={
+    context:{
+        cards:[
+            {
+                subtitle1:"LOREM IPSUM",
+                p1:"Our 500-550 fill power down provides premium warmth at an affordable price.",
+            },
+            {
+                subtitle1:"LOREM IPSUM",
+                p1:"A high-quality down with plump and resilient clusters. 600-650 fill power provides warmth and wonderful loft.",
+            }
+            ,
+            {
+                subtitle1:"LOREM IPSUM",
+                p1:"Our most luxurious down from select cold climate geese. 700-750 fill power Hungarian goose down provides the largest down clusters with exceptional loft.",
+            }
+        ]
+    }
+}
