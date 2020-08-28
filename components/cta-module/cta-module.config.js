cd7801a16f852c21089689464acd0a5395eb9a44
dJimenez04 <djimenez@unitedvirtualities.com>
2020-08-28 15:50:51 -0600

    [TCS-5] adding component to main page

--- components/cta-module/cta-module.config.js
+++ components/cta-module/cta-module.config.js
@@ -3,16 +3,16 @@ module.exports={
         cards:[
             {
                 subtitle1:"LOREM IPSUM",
-                p1:"Our 500-550 fill power down provides premium warmth at an affordable price.",
+                p1:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
             },
             {
                 subtitle1:"LOREM IPSUM",
-                p1:"A high-quality down with plump and resilient clusters. 600-650 fill power provides warmth and wonderful loft.",
+                p1:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
             }
             ,
             {
                 subtitle1:"LOREM IPSUM",
-                p1:"Our most luxurious down from select cold climate geese. 700-750 fill power Hungarian goose down provides the largest down clusters with exceptional loft.",
+                p1:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
             }
         ]
     }
