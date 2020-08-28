0529532331363a64d528c3f132473ad046bd159f
sebastian correa <sebastian@sawyereffect.com>
2020-08-28 19:18:13 -0400

    collapse cards

---
+++ components/certification/certification.config.js
@@ -0,0 +1,22 @@
+module.exports ={
+    context:{
+        cards:[
+            {
+                id:"card1",
+                title:"The Responsible Down Standard (RDS)",
+                text:"Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident."
+            },
+            {
+                id:"card2",
+                title:"STANDARD 100 by OEKO-TEX®",
+                text:"Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident."
+            }
+            ,
+            {
+                id:"card3",
+                title:"Lifetime Guarantee",
+                text:"Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident."
+            }
+        ]
+    }
+}
