b49617e34c8c523d34051f50619981fff9e7b494
sebastian correa <sebastian@sawyereffect.com>
2020-09-01 10:11:32 -0400

    clean on page

---
+++ components/simple-text/simple-text.config.js
@@ -0,0 +1,22 @@
+module.exports ={
+    context:{
+        cards:[
+            {
+                id:"card1c",
+                title:"For Machine Wash Only comforters",
+                text:"Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident."
+            },
+            {
+                id:"card2c",
+                title:"For Dry Clean Only comforters",
+                text:"Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident."
+            }
+            ,
+            {
+                id:"card3c",
+                title:"How to Properly Store a Comforter",
+                text:"Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident."
+            }
+        ]
+    }
+}
