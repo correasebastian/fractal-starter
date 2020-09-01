7f9e07eee443f0197a8d6518ad59b7be8fae4cc1
sebastian correa <sebastian@sawyereffect.com>
2020-09-01 10:25:27 -0400

    faq ready

--- components/simple-text/simple-text.config.js
+++ components/simple-text/simple-text.config.js
@@ -1,22 +1,74 @@
 module.exports ={
     context:{
-        cards:[
-            {
-                id:"card1c",
-                title:"For Machine Wash Only comforters",
-                text:"Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident."
-            },
-            {
-                id:"card2c",
-                title:"For Dry Clean Only comforters",
-                text:"Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident."
+    },
+    variants:[
+        {
+            name:"clean",
+            context:{
+                cards:[
+                    {
+                        id:"card1c",
+                        title:"For Machine Wash Only comforters",
+                        text:"Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident."
+                    },
+                    {
+                        id:"card2c",
+                        title:"For Dry Clean Only comforters",
+                        text:"Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident."
+                    }
+                    ,
+                    {
+                        id:"card3c",
+                        title:"How to Properly Store a Comforter",
+                        text:"Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident."
+                    }
+                ]
             }
-            ,
-            {
-                id:"card3c",
-                title:"How to Properly Store a Comforter",
-                text:"Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident."
+        },
+        {
+            name:"faq",
+            context:{
+                cards:[
+                    {
+                        id:"card1f",
+                        title:"What is the difference between a comforter and duvet insert?",
+                        text:"Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident."
+                    },
+                    {
+                        id:"card2f",
+                        title:"What are the different kinds of down feathers?",
+                        text:"Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident."
+                    }
+                    ,
+                    {
+                        id:"card3f",
+                        title:"How to Properly Store a Comforter",
+                        text:"Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident."
+                    },
+                    {
+                        id:"card4f",
+                        title:"Is your down hypoallergenic? ",
+                        text:"Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident."
+                    },
+                    {
+                        id:"card5f",
+                        title:"What does RDS certified mean?",
+                        text:"Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident."
+                    },
+                    {
+                        id:"card6f",
+                        title:"What does Standard 100 by OEKO-TEX ® mean?",
+                        text:"Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident."
+                    },
+                ]
+
             }
-        ]
-    }
+        },
+        {
+            name:"rviews",
+            context:{
+
+            }
+        },
+    ]
 }
