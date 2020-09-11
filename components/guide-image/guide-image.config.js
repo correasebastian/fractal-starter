86f9e6654d0443462945536c9aacf9243c78d665
dJimenez04 <djimenez@unitedvirtualities.com>
2020-09-11 12:53:22 -0600

    guide component

--- components/guide-image/guide-image.config.js
+++ components/guide-image/guide-image.config.js
@@ -5,7 +5,7 @@ module.exports = {
         title: "What’s the difference between a Comforter and a Duvet Insert?",
         mainImage: "https://res.cloudinary.com/dg1knuetg/image/upload/w_1200,ar_1:1,c_fill,g_auto/v1598412568/duvet_w1xhq3.png",
         richText: `
-            <p >
+            <p>
                 Did you know that comforters and duvet inserts are essentially the same thing? Both are typically filled with down, feathers, or synthetic fibers, will keep you warm, and can be used with our duvet covers which include an array of fun solid colors, fashionable prints, and premium fabrics. Duvet covers can also be interchanged frequently, are easily washed, and protect your investment so it’s a win-win.  
                 <br>All our comforters (and duvet inserts) have handy loops on all four corners that attach to our duvet cover ties to easily secure both in place.
             </p>
