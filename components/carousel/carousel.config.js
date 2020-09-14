93d1654f6ec256483e54e732e109bf9b199d87ea
dJimenez04 <djimenez@unitedvirtualities.com>
2020-09-14 15:15:57 -0600

    fixing button size and font

--- components/carousel/carousel.config.js
+++ components/carousel/carousel.config.js
@@ -3,13 +3,16 @@ module.exports={
     context:{
         slides:[
             {
-                img:"https://res.cloudinary.com/dg1knuetg/image/upload/w_992,ar_1:1,c_fill/v1598883146/car-1.png"
+                img:"https://res.cloudinary.com/dg1knuetg/image/upload/w_992,ar_1:1,c_fill/v1598883146/car-1.png",
+                subtitle:"Sheet Guide"
             },
             {
-                img:"https://res.cloudinary.com/dg1knuetg/image/upload/w_992,ar_1:1,c_fill/v1598883155/car-2.png"
+                img:"https://res.cloudinary.com/dg1knuetg/image/upload/w_992,ar_1:1,c_fill/v1598883155/car-2.png",
+                subtitle:"Pillow Guide"
             },
             {
-                img:"https://res.cloudinary.com/dg1knuetg/image/upload/w_992,ar_1:1,c_fill/v1598883165/car-3.png"
+                img:"https://res.cloudinary.com/dg1knuetg/image/upload/w_992,ar_1:1,c_fill/v1598883165/car-3.png",
+                subtitle:"Sleep Better Guide"
             },
         ]
     },
