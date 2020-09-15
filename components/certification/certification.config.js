3638238f75b6e8104a3362a6e2c1854213896ace
Jesús Hernández <jesus.hernandezmoreno@outlook.com>
2020-09-14 19:00:29 -0500

    Fix: Adding dynamic class on accordion icon  :bug:

--- components/certification/certification.config.js
+++ components/certification/certification.config.js
@@ -1,23 +1,28 @@
-module.exports ={
-    context:{
-        cards:[
-            {
-                id:"card1",
-                class:"show",
-                title:"The Responsible Down Standard (RDS)",
-                text:"Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident."
-            },
-            {
-                id:"card2",
-                title:"STANDARD 100 by OEKO-TEX®",
-                text:"Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident."
-            }
-            ,
-            {
-                id:"card3",
-                title:"Lifetime Guarantee",
-                text:"Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident."
-            }
-        ]
-    }
-}
+module.exports = {
+  context: {
+    cards: [
+      {
+        id: "card1",
+        class: "show",
+        iconClass: "fa fa-minus",
+        title: "The Responsible Down Standard (RDS)",
+        text:
+          "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.",
+      },
+      {
+        id: "card2",
+        iconClass: "fa fa-plus",
+        title: "STANDARD 100 by OEKO-TEX®",
+        text:
+          "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.",
+      },
+      {
+        id: "card3",
+        iconClass: "fa fa-plus",
+        title: "Lifetime Guarantee",
+        text:
+          "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.",
+      },
+    ],
+  },
+};
