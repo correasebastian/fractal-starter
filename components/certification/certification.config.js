8dc61035a1bc9a72277df4b62e496f42c6763353
Jesús Hernández <jesus.hernandezmoreno@outlook.com>
2020-09-15 19:20:33 -0500

    Hotfix: Adding icons based on css  :fire:

--- components/certification/certification.config.js
+++ components/certification/certification.config.js
@@ -4,21 +4,18 @@ module.exports = {
       {
         id: "card1",
         class: "show",
-        iconClass: "fa fa-minus",
         title: "The Responsible Down Standard (RDS)",
         text:
           "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.",
       },
       {
         id: "card2",
-        iconClass: "fa fa-plus",
         title: "STANDARD 100 by OEKO-TEX®",
         text:
           "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.",
       },
       {
         id: "card3",
-        iconClass: "fa fa-plus",
         title: "Lifetime Guarantee",
         text:
           "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.",
