0919b4e624389a4d48eb21e9542d03167ce53af5
dJimenez04 <djimenez@unitedvirtualities.com>
2020-10-22 13:35:33 -0600

    the guide component

---
+++ components/the-guide/the-guide.config.js
@@ -0,0 +1,31 @@
+module.exports ={
+    context:{
+        option:[
+            {   
+                icon:"https://res.cloudinary.com/dg1knuetg/image/upload/v1598649634/icon_vto6g3.png",
+                title:'WARM COMFORT',
+              
+            },
+            {   
+                icon:"https://res.cloudinary.com/dg1knuetg/image/upload/v1598649634/icon_vto6g3.png",
+                title:'TEMPERATURE REGULATING',
+              
+            },
+            {   
+                icon:"https://res.cloudinary.com/dg1knuetg/image/upload/v1598649634/icon_vto6g3.png",
+                title:'SUPPORT YOURSELF',
+              
+            },
+            {   
+                icon:"https://res.cloudinary.com/dg1knuetg/image/upload/v1598649634/icon_vto6g3.png",
+                title:'SUPER SOFT',
+              
+            },
+            {   
+                icon:"https://res.cloudinary.com/dg1knuetg/image/upload/v1598649634/icon_vto6g3.png",
+                title:'5-STAR LUXURY',
+              
+            }
+        ]
+    }
+}
