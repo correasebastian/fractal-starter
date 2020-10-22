0c68953c88a54035734df52cf39573ea6b72e445
emayo18 <jmayo@unitedvirtualities.com>
2020-10-22 16:06:12 -0600

    [TCG-51] Updated component properties

---
+++ components/warm-comfort/warm-comfort.config.js
@@ -0,0 +1,37 @@
+module.exports ={
+    context: {
+        imageHero: [
+            {
+                id: 'heroMobile',
+                className: 'warm-comfort__hero-img',
+                url: 'https://res.cloudinary.com/dg1knuetg/image/upload/v1598650527/TCSSeal-SmallScalePositive_di4zva.png',
+                altText: 'Hero Img Mobile'
+            }
+
+        ],
+
+        imageLogo: [
+            {
+                id: 'heroMobile',
+                className: 'icons-warm',
+                url: 'https://res.cloudinary.com/dg1knuetg/image/upload/v1598650527/TCSSeal-SmallScalePositive_di4zva.png',
+                altText: 'Hero Img Mobile'
+            }
+        ],
+
+        copyTextHeader: 'For the Always Chilly Sleeper',
+        copyTextBody: 'Too cold to snooze? Just layer breathable fabrics like flannel and wool—they retain heat without making you sweat, keeping you warm and comfy all night.',
+        copyLinkText: 'SHOP NOW',
+        copylinkUrl: '#',
+        expertTipHeader: '',
+        experTipBody: 'It’s normal to feel colder at night because your body temperature drops while you sleep. Try a cup of warm tea to increase your core temperature— and create layers of bedding to insulate your body.',
+        
+        imageAsset: [
+            {
+                id: 'imageAsset',
+                url: 'https://res.cloudinary.com/dg1knuetg/image/upload/v1598650527/TCSSeal-SmallScalePositive_di4zva.png',
+                altText: 'Asset'
+            }
+        ],
+    }
+}
