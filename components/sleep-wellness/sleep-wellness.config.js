2bbfc68f2befe23abba417b26147ebafe8c607a3
emayo18 <jmayo@unitedvirtualities.com>
2020-10-22 12:06:13 -0600

    [TCG-49] Created component for mobile and desktop

---
+++ components/sleep-wellness/sleep-wellness.config.js
@@ -0,0 +1,50 @@
+module.exports ={
+    context: {
+        imageHeroBanner: [
+            {
+                id: 'heroMobile',
+                className: 'img-fit d-md-none',
+                url: '/public/images/1001_02_SoftBedding_1263.png',
+                altText: 'Hero Img Mobile'
+            },
+            {
+                id: 'heroTablet',
+                className: 'img-fit d-none d-md-block d-lg-none',
+                url: '/public/images/1001_02_SoftBedding_12632x.png',
+                altText: 'Hero Img Mobile'
+            },
+            {
+                id: 'heroDesktop',
+                className: 'img-fit d-none d-lg-block',
+                url: '/public/images/1001_02_SoftBedding_12632x.png',
+                altText: 'Hero Img Mobile'
+            }
+        ],
+        imageLogo: [
+            {
+                id: 'logoMobile',
+                className: 'd-md-none',
+                url: 'public/images/SW_logo.png',
+                altText: 'Logo Mobile'
+            },
+            {
+                id: 'logoTablet',
+                className: 'd-none d-md-inline d-lg-none',
+                url: 'public/images/SW_logo2x.png',
+                altText: 'Logo Tablet'
+            },
+            {
+                id: 'logoDesktop',
+                className: 'd-none d-lg-inline',
+                url: 'public/images/SW_logo3x.png',
+                altText: 'Logo Desktop'
+            }
+        ],
+        copyText: [
+            {
+                header: 'Better Sleep, Better Life',
+                body: 'No matter what kind of sleeper you are or maybe have on your gift list this year, we’ve created this guide to help you and your loved ones wake up on the right side of bed every day.'
+            }
+        ]
+    }
+}
